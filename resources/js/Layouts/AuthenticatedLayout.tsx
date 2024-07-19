a70765486e21705b37b10087700891deaf2dd4a4
Wisdom Amuzu <wisdomamuzu7@gmail.com>
2024-07-19 16:58:55 +0000

    Add Chat route

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -57,6 +57,7 @@ export default function Authenticated({ user, header, children }: PropsWithChild
 
                                     <Dropdown.Content>
                                         <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
+                                        <Dropdown.Link href={route('chat')}>Chat</Dropdown.Link>
                                         <Dropdown.Link href={route('logout')} method="post" as="button">
                                             Log Out
                                         </Dropdown.Link>
