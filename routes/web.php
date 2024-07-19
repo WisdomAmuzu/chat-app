4a4c2170290b793162ad3010b6ea443838ba2c94
Wisdom Amuzu <wisdomamuzu7@gmail.com>
2024-07-19 16:58:28 +0000

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/chat', [ChatController::class, 'index'])->name('chat');
+    Route::post('/chat/send', [ChatController::class, 'sendMessage']);
 });
 
-Route::get('/chat', [ChatController::class, 'index'])->middleware(['auth']);
-Route::post('/chat/send', [ChatController::class, 'sendMessage'])->middleware(['auth']);
+
 
 require __DIR__.'/auth.php';
